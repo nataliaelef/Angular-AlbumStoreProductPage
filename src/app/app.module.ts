9bc64f72d7e084b1d5f1390d2cec5ad250731611
Natali Eleftheriou <neleftdev@DEV1.ajbell.com>
2019-03-18 08:30:55 +0000

    completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,18 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { RouterModule, Routes } from '@angular/router';
+// import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes: Routes = [
-  { path: 'products', component: ProductListComponent },
-  { path: 'product/:id', component: ProductPageComponent },
-  { path: '', redirectTo: 'products', pathMatch: 'full' }
-];
+// const appRoutes: Routes = [
+//   { path: 'products', component: ProductListComponent },
+//   { path: 'product/:id', component: ProductPageComponent },
+//   { path: '', redirectTo: 'products', pathMatch: 'full' }
+// ];
 
 @NgModule({
 	declarations: [ AppComponent, ProductPageComponent, ProductDescriptionComponent, ProductTracklistingComponent, ProductListComponent ],
-	imports: [ BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(appRoutes) ],
+  imports: [ BrowserModule, FormsModule, HttpModule ],
+  // , RouterModule.forRoot(appRoutes)
 	providers: [ ProductService ],
 	bootstrap: [ AppComponent ]
 })
